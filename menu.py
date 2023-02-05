cba8fb24ab409ea4f105e0c27202e0d3090ed941
Rene Fuentes <jfetff@hotmail.com>
2023-02-05 03:02:18 +0100

    Last combinations until today uploaded.

--- menu.py
+++ menu.py
@@ -2,13 +2,19 @@ import archivos
 
 
 
-
-
 def option1():
     archivos.writefinalfile()
 
+
 def option2():
     archivos.iniciar()
+
+
+def salir():
+    print('\n')
+    print("          Saliendo ......")
+    print("")
+    print("          That is all folks!\n\n")
 
 
 #  Creando el menu y las opciones
@@ -16,11 +22,14 @@ def option2():
 def initial_menu():
     flag = True
     while flag:  
-        print("\n        MAIN MENU")  
+        print("\n") 
+        print("          MAIN MENU")
+        print("")
         print("       1. Load all new combinations so far.")  
-        print("       2. Choose the base of combinations (2 or 3).")  
-        print("       3. Exit")  
-        choice = int(input("       Enter the Choice:"))  
+        print("       2. Choose the base of combinations (2 or 3).") 
+        print("       3. Exit") 
+        print("")
+        choice = int(input("          Enter the Choice: "))  
     
         if choice == 1:
             option1()
@@ -28,65 +37,18 @@ def initial_menu():
             option2()  
         elif choice == 3:  
             flag = False
+            salir()
         else:  
             print("       Oops! Incorrect Choice, try again")
 
 
 # Initial Code - Here is where everything starts
-# if __name__ == '__main__':
-    initial_menu()
-    # leerorigen()
-    # writefinalfile()
-    # quecuantos()
-    print("    That is all folks!!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def menu_principal():
-    print(' \n')
-    opciones = {
-        '1': ('   Opción 1', accion1),
-        '2': ('   Opción 2', accion2),
-        '3': ('   Opción 3', accion3),
-        '4': ('   Salir', salir)
-    }
-
-
-def accion1():
-    print('\n      Has elegido la opción 1\n\n')
-
-
-def accion2():
-    print('\n      Has elegido la opción 2\n\n')
-
-
-def accion3():
-    print('\n      Has elegido la opción 3\n\n')
-
-
-def salir():
-    print('\n      Saliendo ......\n')
-
 if __name__ == '__main__':
-    # Imprimiendo el encabezado inicial  
+    # Imprimiendo el encabezado inicial 
+    print("\n\n")
     print("       ***********************************************")  
     print("                WELCOME TO WIN 'LA PRIMITIVA'")  
     print("       ***********************************************")   
     print("")   
-    menu_principal()
-    print("       That is all folks")
+    initial_menu()
+    
